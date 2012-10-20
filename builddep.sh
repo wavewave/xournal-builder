e2150ddd3106e7a1f5dc670a6ef32c606d71e945
Ian-Woo Kim <ianwookim@gmail.com>
2012-10-19 22:27:17 -0400

    cloneall and then bootstrap

--- builddep.sh
+++ builddep.sh
@@ -2,7 +2,7 @@
 
 mkdir deps
 git clone https://github.com/wavewave/devadmin.git deps/devadmin
-git clone https://github.com/wavewave/xournal-types.git deps/xournal-builder
 cd deps/devadmin ; cabal install ; cd ../../
+$HOME/.cabal/bin/build cloneall --config=build.conf
 $HOME/.cabal/bin/build bootstrap --config=build.conf
 
