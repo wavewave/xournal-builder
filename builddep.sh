67fc19952f8f2fd99daac5f570fb85d120fd3e1c
Ian-Woo Kim <ianwookim@gmail.com>
2012-10-19 22:00:15 -0400

    configure, build to install in builddep.sh

--- builddep.sh
+++ builddep.sh
@@ -3,6 +3,6 @@
 mkdir deps
 git clone https://github.com/wavewave/devadmin.git deps/devadmin
 git clone https://github.com/wavewave/xournal-types.git deps/xournal-builder
-cd deps/devadmin ; cabal configure ; cabal build ; cd ../../
+cd deps/devadmin ; cabal install ; cd ../../
 deps/devadmin/dist/build/build bootstrap --config=build.conf
 
