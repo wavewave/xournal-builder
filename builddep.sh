d1d1ceece8120295546cd66e9abde7787ddffab9
Ian-Woo Kim <ianwookim@gmail.com>
2012-10-19 21:56:09 -0400

    before_script -> before_install

--- builddep.sh
+++ builddep.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 mkdir deps
-git clone https://github.com/wavewave/devadmin.git deps/devadmin.git
-git clone https://github.com/wavewave/xournal-types.git deps/xournal-builder.git 
+git clone https://github.com/wavewave/devadmin.git deps/devadmin
+git clone https://github.com/wavewave/xournal-types.git deps/xournal-builder
 cd deps/devadmin ; cabal configure ; cabal build ; cd ../../
 deps/devadmin/dist/build/build bootstrap --config=build.conf
 
